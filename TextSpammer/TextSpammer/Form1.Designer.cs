2c67a2fb3f119c54884e4c3ad284f8764af19008
Islam Alchasov <islam.alchasov@elev.it-gymnasiet.se>
2015-05-18 11:56:38 +0200

    Made a "anti spam bot" detection
    Just made so that the bot puts a space at the end so that it isn't too
    obvious.

--- TextSpammer/TextSpammer/Form1.Designer.cs
+++ TextSpammer/TextSpammer/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -49,7 +50,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(160, 12);
+            this.button2.Location = new System.Drawing.Point(93, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -75,7 +76,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(12, 156);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.Size = new System.Drawing.Size(237, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox1
@@ -83,14 +84,25 @@
             this.textBox1.Location = new System.Drawing.Point(12, 41);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(223, 92);
+            this.textBox1.Size = new System.Drawing.Size(237, 92);
             this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Other";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(247, 188);
+            this.ClientSize = new System.Drawing.Size(262, 188);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -111,6 +123,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- TextSpammer/TextSpammer/Form1.cs
+++ TextSpammer/TextSpammer/Form1.cs
@@ -13,19 +13,16 @@ namespace TextSpammer
 {
     public partial class Form1 : Form
     {
+        //this is the most important bit for the anti bot thing.
+        bool test;
+        bool btnSec;
+        int Delay;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
+            //I though that it was anyoing when the timer stared blank and everything crashed.
+            textBox2.Text = "1";
         }
         
         //Start Button(Enables the spammer)
@@ -40,19 +37,38 @@ namespace TextSpammer
             Spammer.Enabled = false;
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            test = true;
+            btnSec = true;
+        }
+
         private void Spammer_Tick(object sender, EventArgs e)
         {
             //Gets a custom number for the delay
-            int Delay;
             Delay = Convert.ToInt32(textBox2.Text);
+
+            //This loop does the same thing as the bit below, but it adds a space at the end.
+            //I did this so that anti spam bots don't notice too easily.
+            while (test)
+            {
+                SendKeys.Send(textBox1.Text + " Worked");
+                SendKeys.Send("{ENTER}");
+                test = false;
+            }
 
             //Takes the text in text pox and sends the text to whatever you are using.
             SendKeys.Send(textBox1.Text);
             SendKeys.Send("{ENTER}");
 
+            //If the user presed the other button this will make it work.
+            if (btnSec)
+            {
+                test = true;
+            }
+
             //The delay
             Thread.Sleep(Delay * 1000);
         }
-
     }
 }
